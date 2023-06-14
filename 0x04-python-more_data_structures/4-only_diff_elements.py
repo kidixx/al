da24a9fd6771861b0f309eda908be6a5ec5675bc
kidixx <imop0316@gmail.com>
2023-06-14 13:17:37 -0700

    done 4

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+
+
+
+def only_diff_elements(set_1, set_2):
+
+    """
+    # Write a function that returns a set of all
+    # elements present in only one set.
+    # VARIABLE(" "):
+    # only_diff_elements(set): Only differents
+    """
+
+    diff_set = set()
+
+    #  This line will iterate over the elements in set_1
+    for element in set_1:
+
+        #  This will add the element to diff_set if it's not in set_2
+        if element not in set_2:
+            diff_set.add(element)
+
+    #  will iterate over the element in set_2
+    for element in set_2:
+
+        #  it will then add the element to diff_set
+        #  If it's not in set_1
+        if element not in set_1:
+            diff_set.add(element)
+
+    return diff_set
+
+
+#  Take for instance
+#  set_1 = {"Python", "C", "Javascript"}
+#  set_2 = {"Bash", "C", "Ruby", "Pearl"}
+#  od_set = only_diff_elements(set_1, set_2)
+#  print(od_set)i
