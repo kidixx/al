90a9c64d5231ee31d0d1c96367815d5371fa49b5
kidixx <imop0316@gmail.com>
2023-06-27 10:28:03 -0700

    done

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def raise_exception():
+
+    """
+    # Write a function that raises a type exception.
+    # VARIABLE(" "):
+    # raise_exception(NULL): Raise exception
+    # You are not allowed to import any module
+    # Return: Always 0. (Success)
+    """
+    """ In this function, we simply raise a 'TypeError' exception without...
+    ...any additional information or message."""
+    raise TypeError
