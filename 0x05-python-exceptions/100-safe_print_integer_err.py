b78d7193bb4061ed4e7b96d5cb24ac971967efbf
kidixx <imop0316@gmail.com>
2023-06-27 10:43:46 -0700

    ??

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+
+    """
+    # Write a function that prints an intege)
+    # value can be any type (integer, string, etc.)
+    # The integer should be printed followed by a new line
+    # VARIABLE(" "):
+    # Safe_print_integer(int): Safe integer print with error message
+    # Returns:True if value has been correctly printed
+    # Otherwise, False and prints in stderr the error precede by Exception:
+    """
+    try:
+        """ use a 'try' block to attempt to format and
+        print the value as an integer using """
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as error:
+        """ If the formatting and printing are successful, we return True."""
+        sys.stderr.write("Exception: {}\n".format(error))
+        """ We then use sys.stderr.write to write the...
+        error message to the standard error stream (sys.stderr)."""
+        """" we return 'False' to indicate that an exception..."""
+        """occurred during the printing proces."""
+        return False
