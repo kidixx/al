8804ccace7dab0ded4a3ee2b733b7f2e0c32fa2c
kidixx <imop0316@gmail.com>
2023-06-27 10:59:45 -0700

    done

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def magic_calculation(a, b):
+
+    """
+    # that does exactly the same as the following Python bytecode
+    # VARIABLE(" "):
+    # mafic_calculation(int): ByteCode -> Python #4
+    # Returns: Always 0. (Success)
+    """
+    total_sum = 0
+    for x in range(1, 3):
+        """The byte code represent a Loop that iterates over range '(1, 4)"""
+        try:
+            if x > a:
+                """Inside this Loop, there is a comparison 'if x > a'"""
+                """And if it's true, an exception is raised,
+                Otherwise, the calculation below"""
+                raise Exception('Too Far')
+            total_sum += (a ** b) / x
+        except BaseException:
+            total_sum = b + a
+            break
+        """ Finally, the 'total_sum' is returned"""
+    return total_sum
