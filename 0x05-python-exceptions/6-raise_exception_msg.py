1c7ac2dcd10fe6d00eb8246151a708bcbaf21791
kidixx <imop0316@gmail.com>
2023-06-27 10:38:13 -0700

    done 6

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+
+    """
+    # Write a function that raises a name exception with a message.
+    # VARIABLE(" "):
+    # raise_exception_msg(NULL): Raise a message
+    # You are not allowed to import any module
+    # Return: Always 0. (Success)
+    """
+    """ In this function, we raise a 'NameError' exception with the provided
+    ....'message'. if no message is provided, it will raise a 'NameError'
+    ....without any specific message...."""
+    raise NameError(message)
