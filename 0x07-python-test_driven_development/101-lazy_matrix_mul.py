8fdc928ec6a4cb86bf31898a42ea4af309eabc0a
kidixx <imop0316@gmail.com>
2023-07-04 15:03:00 -0700

    done 7u

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Numpy Module
+"""
+import numpy
+
+'''
+File_name: 101-lazy_matrix_mul.py
+My file 
+Owner kidixx.
+'''
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    # Write a function that multiplies 2 matrices by using the module NumPy...
+    # Test cases should be the same as 100-matrix_mul but with....
+    # new exception type/message
+    # VARIABLE(" "):
+    # Lazy_Numpy: Lazy matrix multiplication
+    # Return: Always 0. (Success).
+    """
+    return numpy.matmul(m_a, m_b)
--- 0x07-python-test_driven_development/README.md
+++ 0x07-python-test_driven_development/README.md
@@ -1,1 +1,1 @@
-my fike
+readme
