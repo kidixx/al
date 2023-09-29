ce7ee87e8a752da257da54e4cdb8bbed773361bf
kidixx <imop0316@gmail.com>
2023-09-29 21:17:47 +0100

    done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-# Gets the size of the body of a response from a URL
-curl -sI "$1" | grep -iE 'Content-Length:' | awk '{print $2}'
+
+# Check if the user provided a URL as an argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Store the URL provided by the user
+url="$1"
+
+# Use curl to fetch the URL and store the response in a variable
+response=$(curl -s "$url")
+
+# Get the size of the response body in bytes
+size=$(echo -n "$response" | wc -c)
+
+# Display the size of the response body
+echo "Size of the response body: $size bytes"
